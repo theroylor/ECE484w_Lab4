a7b40f24f4eccb13c47168ee0f3da1b7aecaedc6
Taylor Roy <34606532+theroylor@users.noreply.github.com>
2025-03-26 13:04:57 -0400

    Update lab4.cpp

--- DE10/lab4.cpp
+++ DE10/lab4.cpp
@@ -7,6 +7,48 @@
 #include <opencv2/opencv.hpp>
 #include <cstdint> // For fixed-width integer types like uint32_t
 #include <cstdlib> // For std::memcpy
+#include <unordered_map>
+
+cv::Mat imageBase, imageOverlay;
+int32_t brightness,contrast,mode=-1;
+
+struct Packet {
+    uint32_t sequenceNumber;
+    uint32_t totalChunks;
+    uint32_t dataSize;
+    std::vector<char> data; // Data payload
+};
+
+std::unordered_map<uint32_t, std::map<uint32_t, Packet>> messages;
+
+void setMode(uint32_t command)
+{
+	mode = command;
+}
+
+void modeBrightnessContrast()
+{
+	
+}
+
+void modeOverlay()
+{
+	
+}
+
+void receivePackets(uint32_t messageId, const Packet& packet) {
+    messages[messageId][packet.sequenceNumber] = packet;
+}
+
+bool recievedAllChunks(uint32_t messageId) {
+    auto& chunks = messages[messageId];
+    if (chunks.size() == chunks.begin()->second.totalChunks) {
+        return true;
+    }
+    return false;
+}
+
+
 
 // Function to handle an image message
 void processImageMessage(const std::vector<unsigned char> &data) {
@@ -18,15 +60,25 @@ void processImageMessage(const std::vector<unsigned char> &data) {
     }
 
     // Display the image
-    cv::imshow("Received Image", image);
-    cv::imwrite("received_image.jpg", image); // Save image to file
+    cv::imshow("Base Image", image);
+    cv::imwrite("Base_Image.BMP", image); // Save image to file
     cv::waitKey(0); // Wait for the user to close the window
     std::cout << "Image processed and saved!" << std::endl;
 }
 
+void displayBaseImage(String title)
+{
+    cv::imshow(title, image);
+    //cv::imwrite("Base_Image.BMP", image); // Save image to file
+    //cv::waitKey(0); // Wait for the user to close the window
+    //std::cout << "Image processed and saved!" << std::endl;
+}
+
 // Function to handle an integer message
 void processIntegerMessage(uint32_t ID,uint32_t value) {
-    std::cout << "Received Integer at " << std::hex << ID << " with value : " << std::dec << value << std::endl;
+	uint32_t integerValue;
+	std::memcpy(&integerValue, &buffer[16], sizeof(uint32_t));
+    std::cout << "Received Integer at " << std::hex << ID << " with value : " << std::dec << integerValue << std::endl;
     // Perform any additional logic you want for integers
 }
 
@@ -61,7 +113,10 @@ int main() {
 
         if (bytes_received > 0) {
             buffer[bytes_received] = '\0'; // Null-terminate the received data if needed
-
+			std::memcpy(&messageId, &buffer[0], sizeof(uint32_t));			
+			std::memcpy(&messageId, &buffer[0], sizeof(uint32_t));			
+			
+			receivePackets()
             // Extract `messageID` (first 4 bytes)
 			uint32_t messageId;
 			std::memcpy(&messageId, &buffer[0], sizeof(uint32_t));			
@@ -80,9 +135,7 @@ int main() {
 				//break;
 				case 0x2C:
 				{
-				uint32_t integerValue;
-				std::memcpy(&integerValue, &buffer[16], sizeof(uint32_t));
-				std::cout << "Processing message at " << std::hex << messageId << std::dec << " : " << integerValue << std::endl;
+
 								
 				}
 				// Extract and process integer (bytes 4–8, offset 12 for data)
